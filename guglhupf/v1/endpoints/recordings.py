696900bf0d0b2feaab712af5c373baeef351d2e1
Frank Blechschmidt <contact@frank-blechschmidt.com>
2021-05-04 22:40:16 -0700

    Stream video recordings to frontend

--- guglhupf/v1/endpoints/recordings.py
+++ guglhupf/v1/endpoints/recordings.py
@@ -1,6 +1,7 @@
 import fnmatch
+from pathlib import Path
 
-from fastapi import Query
+from fastapi import Query, Request
 from fastapi.responses import StreamingResponse
 
 from guglhupf.core.settings import settings
@@ -12,13 +13,36 @@ router = APIRouter(
     responses={404: {'description': 'Not found'}},
 )
 
+BYTES_PER_RESPONSE = 100000
+
+
+def get_video_and_total_size(video):
+    file_path = Path(settings.recordings_dir / video)
+    video_file = open(file_path, mode='rb')
+    video_size = (file_path).stat().st_size
+    return video_file, video_size
+
+
+def chunk_generator_from_stream(stream, chunk_size, start, size):
+    bytes_read = 0
+
+    stream.seek(start)
+
+    while bytes_read < size:
+        bytes_to_read = min(chunk_size,
+                            size - bytes_read)
+        yield stream.read(bytes_to_read)
+        bytes_read = bytes_read + bytes_to_read
+
+    stream.close()
+
 
 @router.get('/')
 async def list_recordings(
-    front: bool = True,
-    back: bool = True,
-    local: bool = True,
-    cloud: bool = True,
+    front: bool = False,
+    back: bool = False,
+    local: bool = False,
+    cloud: bool = False,
 ):
     with open(settings.uploaded_files_txt) as uploaded_files_txt:
         all_uploaded_videos = {
@@ -51,6 +75,29 @@ async def list_recordings(
 
 
 @router.get('/{video}')
-def list_recordings(video: str = Query(..., regex='^[a-z0-9_-]+\.mp4$')):
-    video_file = open(settings.recordings_dir / video, mode='rb')
-    return StreamingResponse(video_file, media_type='video/mp4')
+def list_recordings(
+    req: Request,
+    video: str = Query(..., regex='^[a-z0-9_-]+\.mp4$'),
+):
+    # https://github.com/tiangolo/fastapi/issues/1240#issuecomment-797618168
+    asked = req.headers.get("Range")
+    stream, total_size = get_video_and_total_size(video)
+    start_byte_requested = int(asked.split("=")[-1][:-1])
+    end_byte_planned = min(start_byte_requested +
+                           BYTES_PER_RESPONSE, total_size) - 1
+    chunk_generator = chunk_generator_from_stream(
+        stream,
+        chunk_size=10000,
+        start=start_byte_requested,
+        size=BYTES_PER_RESPONSE
+    )
+    return StreamingResponse(
+        chunk_generator,
+        headers={
+            "Accept-Ranges": "bytes",
+            "Content-Range": f"bytes {start_byte_requested}-{end_byte_planned}/{total_size}",
+            "Content-Type": "..."
+        },
+        status_code=206,
+        media_type='video/mp4',
+    )
